b682ea91a02575b10ac70968a93c2ce5e3a537e7
KingChlorine <kingchloe281@gmail.com>
2025-11-30 15:15:03 +0000

    earch function working and removed whitespace

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-const dropdown = document.querySelector(".selectCategory");
-const accessories = document.getElementById("accessories");
 
+
+const search = () =>{
+  const searchbox = document.getElementById("searchItems").value.toUpperCase();
+  const storeItems = document.getElementById("listProduct")
+  const product = document.querySelectorAll(".item1")
+  const pname = storeItems.getElementsByTagName("h2")
+
+  for(var i=0; i < pname.length; i++){
+    let match = product[i].getElementsByTagName('h2')[0];
+
+    if(match){
+     let textvalue = match.textContent || match.innerHTML
+     if(textvalue.toUpperCase().indexOf(searchbox) > -1){
+      product[i].style.display = "";
+     }else{ product[i].style.display ="none";
+
+     }
+     }
+    }
+  }
 
  
+
 
 
 // Sign Up button test
